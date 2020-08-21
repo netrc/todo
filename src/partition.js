81388017fbc3c6677807a53cf46cf1a87962c191
Richard Campbell <ric@netrc.com>
2020-08-21 15:43:58 -0400

    with reference back to stack overflow

--- src/partition.js
+++ src/partition.js
@@ -1,5 +1,9 @@
 
+// see https://stackoverflow.com/questions/38860643/split-array-into-two-different-arrays-using-functional-javascript
 
+// g is a collection appender function
+// f is the partition function returning true/false
+// xs is the collection
 exports.partitionCollectionBy = g => f => xs => {
   return xs.reduce((m,x) => {
     let v = f(x)
