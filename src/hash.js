4701bdebe7cd87724605b96ecf0d68a7eb0c5e9f
Richard Campbell <ric@netrc.com>
2020-08-22 15:00:49 -0400

    hash func

---
+++ src/hash.js
@@ -0,0 +1,13 @@
+
+
+var c = require('crypto')
+
+// for full list of digest/hash functions check your platform with
+// $ openssl list -digest-algorithms
+
+
+exports.get = s => {
+  var h = c.createHash('sha256') // 'md5'...
+  h.update(s)
+  return h.digest('base64') // buffer encoding:  hex base64 
+}
