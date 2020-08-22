cdfc46aa1024c2031824178d1c268b30de9e3646
Richard Campbell <ric@netrc.com>
2020-08-22 17:00:21 -0400

    eslinting

--- src/partition.js
+++ src/partition.js
@@ -1,23 +1,18 @@
 
 // see https://stackoverflow.com/questions/38860643/split-array-into-two-different-arrays-using-functional-javascript
-exports.partitionCollectionBy = g => f => xs => {
-  return xs.reduce((m,x) => {
-    let v = f(x)                          
-    return m.set(v, g(m.get(v), x))
-  }, new Map())
-}
 
 // g is a collection appender function
 // f is the partition function returning true/false
 // xs is the collection
 // returns spread of results; each could be undefined
 exports.partitionCollectionBy = g => f => xs => {
-  const a = xs.reduce( (m,x) => {
-    let v = f(x)                          
+  const a = xs.reduce( (m, x) => {
+    const v = f(x)
     return m.set(v, g(m.get(v), x))
-    }, new Map())
+  }, new Map())
   return [ a.get(true), a.get(false) ]
 }
 
-exports.partitionArrayBy = exports.partitionCollectionBy( (a=[],b) => [...a,b] )
+const arrayAppendFunc = (a=[], b) => [ ...a, b ]
+exports.partitionArrayBy = exports.partitionCollectionBy( arrayAppendFunc )
 
--- src/tdates.js
+++ src/tdates.js
@@ -1,10 +1,16 @@
 
-exports.isoToDayStr = s => s.substring(0,10)  // '2020-08-19'
-exports.isoToTimeStr = s => s.substring(11,16)  // '21:04      // WRONG TIME ZONE
+exports.isoToDayStr = s => s.substring(0, 10) // '2020-08-19'
+exports.isoToTimeStr = s => s.substring(11, 16) // '21:04'
 
-const padz = n => n.toString().padStart(2,'0')
+const padz = n => n.toString().padStart(2, '0')
 
 exports.isoStr = () => {
   const d = new Date()
-  return `${d.getFullYear()}-${padz(d.getMonth()+1)}-${padz(d.getDate())}T${padz(d.getHours())}:${padz(d.getMinutes())}:${padz(d.getSeconds())}`
+  const year = d.getFullYear()
+  const month = padz(d.getMonth() + 1)
+  const day = padz(d.getDate())
+  const hours = padz(d.getHours())
+  const mins = padz(d.getMinutes())
+  const secs = padz(d.getSeconds())
+  return `${year}-${month}-${day}T${hours}:${mins}:${secs}`
 }
