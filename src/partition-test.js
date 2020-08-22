14f50a10bc22e018dc336ea9838260469b94c953
Richard Campbell <ric@netrc.com>
2020-08-22 08:38:34 -0400

    partition returns spread struct of both true and false arrays

--- src/partition-test.js
+++ src/partition-test.js
@@ -1,11 +1,21 @@
+const pa = require('./partition')
 
 const ar = [ 1,3,5,6,8,10,14 ]
 
 const evens = n => (n%2==0)
+const paByEvens = pa.partitionArrayBy(evens)
 
-const paByEvens = require('./partition').partitionArrayBy(evens)
+const [ arEvens, arOdds ]  = paByEvens(ar)
+console.log(arEvens)
+console.log(arOdds)
 
-const arEvens = paByEvens(ar)
-console.log(arEvens.get(true))
-console.log(arEvens.get(false))
+console.log('----------')
+
+const hundreds = n => (n>99)
+const paByHundreds = pa.partitionArrayBy(hundreds)
+
+const [ arHundreds, arLess ]  = paByHundreds(ar)
+console.log(arHundreds)
+console.log(arLess)
+
 
--- src/partition.js
+++ src/partition.js
@@ -1,14 +1,22 @@
 
 // see https://stackoverflow.com/questions/38860643/split-array-into-two-different-arrays-using-functional-javascript
+exports.partitionCollectionBy = g => f => xs => {
+  return xs.reduce((m,x) => {
+    let v = f(x)                          
+    return m.set(v, g(m.get(v), x))
+  }, new Map())
+}
 
 // g is a collection appender function
 // f is the partition function returning true/false
 // xs is the collection
+// returns spread of results; each could be undefined
 exports.partitionCollectionBy = g => f => xs => {
-  return xs.reduce((m,x) => {
-    let v = f(x)
+  const a = xs.reduce( (m,x) => {
+    let v = f(x)                          
     return m.set(v, g(m.get(v), x))
-  }, new Map())
+    }, new Map())
+  return [ a.get(true), a.get(false) ]
 }
 
 exports.partitionArrayBy = exports.partitionCollectionBy( (a=[],b) => [...a,b] )
