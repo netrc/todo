28d4e33f5e692d9c16adf73a240155c77644a660
Richard Campbell <ric@netrc.com>
2020-08-22 14:50:50 -0400

    change test ddb.js to ava

---
+++ src/test-ddb.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const test = require('ava')
+
+const ddb = require('./ddb')
+
+test('basic ddb test', async t => {
+  ddb.checkEnv() // Auth:  uses AWS env vars for auth
+
+  ddb.setTable('todo')
+
+  const tval = await ddb.getItem('todo')
+  t.true(tval.length>50)  
+
+  const doneKey = '2020-08-20'
+  const jval = await ddb.getItem(doneKey)
+  t.true(jval.length>10)  
+})
